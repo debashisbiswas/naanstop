577e84ffe8b85327a36d9c189559bea8bc934104
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2023-06-23 21:41:26 -0700

    Complete basic front page design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="grid h-full place-items-center">
+	<div class="text-center">
+		<div class="mb-8">
+			<img
+				src="https://source.unsplash.com/Xnb826JWdDI"
+				alt="Chicken curry"
+				class="mask mask-squircle mb-4 h-96 w-96"
+			/>
+			<p class="text-center text-xl italic text-gray-700">Now taking orders for</p>
+			<h1 class="text-center text-4xl font-bold tracking-tight">Chicken Curry</h1>
+		</div>
+
+		<button class="btn-primary btn">Order now</button>
+	</div>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('daisyui')],
-}
+	content: ['./src/**/*.{html,svelte,ts}'],
+	theme: {
+		extend: {}
+	},
+	plugins: [require('daisyui')],
+	daisyui: {
+		themes: ['autumn']
+	}
+};
